a00f86562403998a67591359bc6935e125833e0f
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2023-02-02 20:53:06 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        double answer1 = (num1 * 1.0) / num2;
+        answer = (int) (answer1 * 1000);
+      
+
+        
+        
+        return answer;
+    }
+}
