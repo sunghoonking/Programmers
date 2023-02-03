3375301a794c1cf324409247ce4878c4d6de9bb6
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2023-02-03 21:40:27 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+class Solution {
+    public int solution(int n) {
+        
+        int answer = 0;   
+    
+        for(int i = 1; i<=n; i++){
+            if(i%2==0){
+                answer += i;
+            }   
+        }
+        
+        return answer;
+    }
+}
