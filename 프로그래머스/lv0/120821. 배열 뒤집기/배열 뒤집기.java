edc41bdff5878de6bea099016f3e054cc79cfce1
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2023-02-04 23:27:25 +0900

    Update 배열 뒤집기.java

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -5,9 +5,7 @@ class Solution {
         answer = new int[num_list.length];
         int w = 0;
         for(int i= num_list.length-1; i >= 0 ;i--){
-            if(w == num_list.length){
-                  break;
-            }
+        
             answer[w] = num_list[i];
             
              w++;
