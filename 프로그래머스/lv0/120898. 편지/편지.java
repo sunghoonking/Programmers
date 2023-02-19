b344a3268fce2b19c48cbbc78b8c571adacdff13
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2023-02-19 20:30:09 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        answer = message.length()  * 2;       
+   
+        
+        
+        return answer;
+    }
+}
