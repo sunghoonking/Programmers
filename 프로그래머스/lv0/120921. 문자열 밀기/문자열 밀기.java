03ce811d3428aef637ab0379dda1ffcb01b87085
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2023-05-15 09:33:35 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,39 +1,9 @@
 class Solution {
     public int solution(String A, String B) {
         int answer = 0;
-        
-               if (A.equals(B)){
-
-            return answer;
-        }
-        while (true) {
-            if (answer>A.length()){
-                answer=-1;
-                break;
-            }
-            String s = arrayToString(A);
-            answer++;
-            A = s;
-            if (s.equals(B)){
-                break;
-            }
-        }
-        
+   
+        String repeat = B.repeat(3);
+        answer = repeat.indexOf(A);
         return answer;
-    }
-    
-      public static String arrayToString(String A){
-        String[] split = A.split("");
-        String a = "";
-        String s = split[split.length - 1];
-        for (int i = split.length-1; i >0 ; i--) {
-            split[i] = split[i-1];
-        }
-        split[0] = s;
-        for (int i = 0; i < split.length ; i++) {
-            a+=split[i];
-        }
-
-        return a;
     }
 }
