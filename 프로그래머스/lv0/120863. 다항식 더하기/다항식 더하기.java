d2dc610a2afc4e0da443651cec61d5415aa3f720
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2023-05-18 09:44:08 +0900

    [level 0] Title: 다항식 더하기, Time: 6.90 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
@@ -38,3 +38,37 @@ class Solution {
         return answer;
     }
 }
+
+//        String answer1 ="";
+//        int a = 0;
+//        int b = 0;
+//        String[] q = polynomial.split(" ");
+//        for (int i = 0; i <q.length; i++) {
+//            String s1 = q[i];
+//            if (s1.contains("x")){
+//                String x = s1.replaceAll("x", "");
+//                if (x.equals("")){
+//                    a+=1;
+//                }else {
+//                    a+= Integer.parseInt(x);
+//                }
+//            } else if (s1.matches("[1-9]+")) {
+//                b+=Integer.parseInt(s1);
+//            }
+//        }
+//        String first = (a) + "x";
+//        String second = String.valueOf(b);
+//        if (a==1){
+//            first = "x";
+//        } else if (a==0) {
+//            first = "0" +"x";
+//        }
+//        if (a!=0 && b!=0){
+//            answer1 = first + " + " + second;
+//        }else if (a==0){
+//            answer1 = second;
+//        }
+//        else if (b==0){
+//            answer1 = first;
+//        }
+//        System.out.println(answer1);
