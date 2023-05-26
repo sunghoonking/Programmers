67bfdaf3776444f336af277cff41ed457a36c56d
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2023-05-26 13:15:40 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.72 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,49 +1,23 @@
 import java.math.BigInteger;
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-         int[] answer = new int[2];
-        int key = 0;
-        int key2 = 0;
-        int bunJa = 0;
-        int bunMo = 0;
-        if (denom1!=denom2){
-            for (int i = 1; i < 1000; i++) {
-                int i1 = denom1 * i;
-                for (int j = 1; j < 1000; j++) {
-                    int i2 = denom2 * j;
-                    if (i1 == i2){
-                        key= i;
-                        key2=j;
-                        break;
-                    }
-                }
-                if (key!=0){
-                    break;
-                }
-            }
-            bunJa = numer1 * key + numer2 * key2;
-           bunMo = denom1 * key;
+              int[] answer = new int[2];
+        numer1 *= denom2;
+        numer2 *= denom1;
 
-        }else {
-            bunJa = numer1+numer2;;
-             bunMo = denom1;
-
-        }
-        BigInteger bj = BigInteger.valueOf(bunJa);
-        BigInteger bm = BigInteger.valueOf(bunMo);
+        int son = numer1+numer2;
+        int mother = denom1 * denom2;
+        BigInteger mo = BigInteger.valueOf(mother);
+        BigInteger so = BigInteger.valueOf(son);
         BigInteger gcd;
-        if (bunJa>bunMo){
-            gcd = bj.gcd(bm);
+        if (mother>son){
+            gcd = mo.gcd(so);
+        }else {
+            gcd = so.gcd(mo);
         }
-        else {
-            gcd = bm.gcd(bj);
-        }
-        
         int i = Integer.parseInt(String.valueOf(gcd));
-        bunJa /= i;
-        bunMo /= i;
-        answer[0] = bunJa;
-        answer[1] = bunMo;
+        answer[0] = son/i;
+        answer[1] = mother/i;
     
         return answer;
     }
