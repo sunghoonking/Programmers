a7495ba8b20bae39afc41b341345a9f409eee48e
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2023-08-31 10:08:57 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.31 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -4,38 +4,11 @@ import java.util.HashMap;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-                String an = "";
-        Map<String, String> map = new HashMap<>();
-        map.put("nine", "9");
-        map.put("eight", "8");
-        map.put("seven", "7");
-        map.put("six", "6");
-        map.put("five", "5");
-        map.put("four", "4");
-        map.put("three", "3");
-        map.put("two", "2");
-        map.put("one", "1");
-        map.put("zero", "0");
-
-        String temp = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) - '0' > 10) {
-                temp += String.valueOf(s.charAt(i));
-            } else {
-                an+= s.charAt(i)-'0';
-                continue;
-            }
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            String key = entry.getKey();
-            String entryValue = entry.getValue();
-            if (temp.equals(key)){
-                an+= entryValue;
-                temp="";
-            }
-         }
+             String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-answer = (int) Long.parseLong(an);
-
+        answer = Integer.parseInt(s);
         return answer;
     }
 }
