2b2883923a228510ef8ffa1f217997a3ffdc02b3
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2025-03-20 21:17:50 +0900

    [Bronze V] Title: 연세대학교, Time: 172 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class Main{
+    public static void main(String[] agrs){
+
+        Scanner scanner = new Scanner(System.in);
+
+        if(scanner.nextInt()==0){
+            System.out.println("YONSEI");
+        }else{
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+
+}
