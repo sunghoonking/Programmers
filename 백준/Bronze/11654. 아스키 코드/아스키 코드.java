5c5cd4316a0d402394d0bcfa3ade9fbebff41b66
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2025-04-12 21:52:11 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String next = in.next();
+
+        int answer = next.charAt(0);
+        System.out.println(answer);
+    }
+}
